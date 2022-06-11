aa5852bed726276783998ed7fe185af78bd365a3
JessGiannini <jessgiannini@gmail.com>
2022-06-11 10:01:07 -0700

    server setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+const PORT = process.env.PORT || 3001
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => { console.log("connection good") })
+    .catch((error) => { console.log(error) })
+
+app.listen(PORT, () => { console.log(`Backend server now running on port http://localhost:${PORT}`) })
