4f08b184ab057e3d9f9170037a2ebe5ec356e613
Josh Gumperz <joshg3023@gmail.com>
2022-06-15 18:51:28 -0700

    made minor styling adjustment to putluck page

--- client/src/pages/potluck.js
+++ client/src/pages/potluck.js
@@ -7,7 +7,7 @@ function Potluck({ updateSubmitted, submitted }) {
   return (
     <div>
       <div className='potluck-header-container'>
-        <h6 className="header-potluck">Date: July 9, 2022 Location: Lake Elizabeth 1 Sailway Dr, Fremont, CA 94538 Time: Noon</h6>
+        <h6 className="header-potluck">Date: July 9, 2022 - Location: Lake Elizabeth 1 Sailway Dr, Fremont, CA 94538 - Time: Noon</h6>
       </div>
       <div className='potluck-container'>
         <div className='potluck-form-container'>
